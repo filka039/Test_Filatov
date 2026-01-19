429f3961760690f62d9ff8458db63672dd6b811c
Filatov Alexander <fil92.92@gmail.com>
2026-01-19 16:29:17 +0200

    Delete src directory

--- src/main/java/org/example/constants/Actions.java
+++
@@ -1,8 +0,0 @@
-package org.example.constants;
-
-public class Actions {
-    public static final String LOGIN = "LOGIN";
-    public static final String ACTION = "ACTION";
-    public static final String LOGOUT = "LOGOUT";
-    public static final String REGISTER = "REGISTER";
-}
--- src/main/java/org/example/constants/Endpoints.java
+++
@@ -1,7 +0,0 @@
-package org.example.constants;
-
-public class Endpoints {
-
-    public static final String BASE_URL = "http://localhost:8080/";
-    public static final String COMMON_ENDPOINT = "endpoint";
-}
